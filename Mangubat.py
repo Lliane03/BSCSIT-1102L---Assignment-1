ddf277a2f365d65c503bbeb2e13eb2b1f4ffbb82
Lliane03 <angeljullianemangubat@gmail.com>
2024-08-28 15:31:53 +0800

    Add files via upload
    BSCSIT 1102L - Assignment
    Submitted by: Angel Julliane I. Mangubat (BSCS-DS)
    Submitted to: Sir Val Patrick

---
+++ Mangubat.py
@@ -0,0 +1,5 @@
+from pyfiglet import figlet_format
+print(figlet_format("Angel", font = "isometric3" ))
+print( figlet_format("Julliane", font = "univers" ) )
+print(figlet_format("Iwag", font = "eftiwater" ))
+print( figlet_format("Mangubat", font = "banner3" ) )
